aea7be047e0db14d12183268c673860a9dac6cd8
Arnold Joseph <arnold.anaviso@gmail.com>
2018-03-27 08:39:37 +0800

    Initial Commit

---
+++ templates/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Bohol Resorts</title>
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" type="text/css" href="../static/css/bootstrap/bootstrap.min.css">
+    <!-- My styles -->
+    <link rel="stylesheet" type="text/css" href="../static/css/style.css">
+    <!-- Fonts -->  
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,300i,400" rel="stylesheet">
+  </head>
+
+  <body>
+		<div class="container">
+      <header>
+          <div class="row">
+            <div class="col">
+              <div class="follow-us-box clearfix">
+                <div class="row">
+                  <div class="col">
+                    <h5 class="follow-us-text">Follow Us:</h5>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col follow-us-icons">
+                    <a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter-square white-icon"></i></a>
+                    <a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-square white-icon"></i></a>
+                    <a href="https://www.instagram.com/?hl=en" target="_blank"><i class="fab fa-instagram white-icon"></i>	</a>
+                  </div>
+                </div>	
+              </div>
+            </div>
+          </div>
+      </header>
+			<section class="section-carousel">
+				<div class="row">
+					<div class="col">
+						<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+						  <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img class="d-block w-100 carousel-img" src="../static/images/carousel-1.jpg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../static/images/carousel-2.jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../static/images/carousel-3.jpg" alt="Third slide">
+                </div>
+						  </div>
+						  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+						  </a>
+						  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+						  </a>
+              <div class="logo">
+                <h2 class="logo-text">Bohol Resorts</h2>
+              </div>
+              <nav>
+                <div class="row">
+                  <ul class="main-nav">
+                    <li><a href="#">Main</a></li>
+                    <li><a href="#">Buying</a></li>
+                    <li><a href="#">Selling</a></li>
+                    <li><a href="#">Renting</a></li>
+                    <li><a href="#">Finance</a></li>
+                    <li><a href="#">Contacts</a></li>
+                  </ul>
+                </div>
+              </nav>
+						</div>						
+					</div>
+				</div>
+			</section>
+      <section class="section-main">
+        <div class="row">
+          <div class="col-md-9">
+            <div class="top-box">
+              <h3>Welcome message!</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper augue ligula, ac posuere mauris eleifend a. In molestie feugiat nunc, in mollis urna feugiat et. Nullam fringilla maximus nisi a porta. Curabitur porta, tortor nec convallis interdum, nisl nulla blandit purus, id iaculis urna orci vel lorem. Praesent venenatis mauris ac erat tincidunt ultrices. Sed id aliquet ante. Proin auctor, nisl ut maximus viverra, nisi mauris ornare purus, pharetra ornare orci dui sed felis. Nulla mattis nunc ut convallis aliquam. Nunc sed nulla augue.
+              </p>
+              <p>
+                Vestibulum efficitur velit quis velit fermentum, eu porttitor ligula vestibulum. Sed eget consectetur nisi. Fusce eget auctor diam. Duis lacus velit, tincidunt in tempus eu, porttitor vitae nisi. Nam faucibus odio velit, eu ultrices enim efficitur sit amet. Donec ut erat viverra, porta lorem sit amet, cursus mauris. Pellentesque euismod in elit id imperdiet. Nullam eu tellus non urna tincidunt varius. Vestibulum commodo nisl vitae dolor dapibus pharetra. Nulla rutrum ac eros et ultrices. Donec ac nulla quam. Vestibulum luctus arcu ipsum, eget molestie sapien auctor sed. Sed malesuada turpis ac dui vestibulum hendrerit. Duis ut nisl vel enim mattis vehicula et venenatis nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+              </p>
+            </div>
+          </div>
+          <div class="col-md-3">
+          </div>
+        </div>
+      </section>
+		</div>
+	<!-- Jquery -->
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+	<!-- Bootstrap -->
+	<script src="../static/js/bootstrap/bootstrap.min.js"></script>
+	<!-- Font Awesome -->
+	<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js" integrity="sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ" crossorigin="anonymous"></script>
+	<!-- My Javascript -->
+  </body>
+</html>
+
